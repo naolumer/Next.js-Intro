3f27972f0999e80fcd72b71bbdb739d33e1b3d03
naolumer <naolumer3@gmail.com>
2025-04-22 21:30:05 +0300

    DUMMY TABLE

--- fullstack-todoapp/app/page.jsx
+++ fullstack-todoapp/app/page.jsx
@@ -7,6 +7,49 @@ export default function Home() {
         <textarea name="description" placeholder="Enter Description" className="px-3 py-2 border-2 border-gray-300 w-full" id=""></textarea>
         <button type="submit" className="bg-orange-600 py-3 px-11 text-white">Add Todo</button>
       </form>
+
+      
+
+      <div className="relative overflow-x-auto mt-24 w-[60%] mx-auto">
+          <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                  <tr>
+                      <th scope="col" className="px-6 py-3">
+                          ID
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                          TITLE
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                          DESCRIPTION
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                          STATUS
+                      </th>
+                      <th scope="col" className="px-6 py-3">
+                          ACTION
+                      </th>
+                  </tr>
+              </thead>
+              <tbody>
+                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                      <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                          Apple MacBook Pro 17"
+                      </th>
+                      <td className="px-6 py-4">
+                          Silver
+                      </td>
+                      <td className="px-6 py-4">
+                          Laptop
+                      </td>
+                      <td className="px-6 py-4">
+                          $2999
+                      </td>
+                  </tr>
+              </tbody>
+          </table>
+      </div>
+
     </>
   );
 }
