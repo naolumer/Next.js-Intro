815a8f700b924b0fe4ca58d70929465bcb9b1d21
naolumer <naolumer3@gmail.com>
2025-04-22 21:37:26 +0300

    added dummy todo task

--- fullstack-todoapp/app/page.jsx
+++ fullstack-todoapp/app/page.jsx
@@ -34,16 +34,20 @@ export default function Home() {
               <tbody>
                   <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
                       <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                          Apple MacBook Pro 17"
+                          2314
                       </th>
                       <td className="px-6 py-4">
-                          Silver
+                          Walk the dogs
                       </td>
                       <td className="px-6 py-4">
-                          Laptop
+                          My day to day task
                       </td>
                       <td className="px-6 py-4">
-                          $2999
+                          Pending
+                      </td>
+                      <td className="px-6 py-4">
+                          <button>Delete</button>
+                          <button>Done</button>
                       </td>
                   </tr>
               </tbody>
