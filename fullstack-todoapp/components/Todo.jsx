5e2989cfab25213a701359abc9c893b926d54c7a
naolumer <naolumer3@gmail.com>
2025-07-10 20:24:34 +0300

    increased text on small screens

--- fullstack-todoapp/components/Todo.jsx
+++ fullstack-todoapp/components/Todo.jsx
@@ -3,16 +3,16 @@ const Todo = ({complete,title,description,id,mongoId,deleteTodo,updateTodo}) => 
   return (
     <>
       <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
-        <th scope="row" className="px-3 py-2 sm:px-6 sm:py-3 font-medium text-[10px] sm:text-[12px] text-gray-900 whitespace-nowrap dark:text-white">
+        <th scope="row" className="px-3 py-2 sm:px-6 sm:py-3 font-medium text-[11px] sm:text-[12px] text-gray-900 whitespace-nowrap dark:text-white">
             {id}
         </th>
         <td className={` ${complete? "line-through":""}  pl-4 py-2 text-[10px] sm:text-[11px] lg:text-[13px] sm:py-3`}>
             {title}
         </td>
-        <td className={` ${complete? "line-through":""}  pl-4 py-2 text-[10px] sm:text-[11px] lg:text-[13px] sm:py-3`}>
+        <td className={` ${complete? "line-through":""}  pl-4 py-2 text-[11px] sm:text-[11px] lg:text-[13px] sm:py-3`}>
             {description}
         </td>
-        <td className="px-3 py-2 text-[10px] sm:text-[11px] lg:text-[13px]  sm:py-3">
+        <td className="px-3 py-2 text-[11px] sm:text-[11px] lg:text-[13px]  sm:py-3">
             {complete? "Completed":"Pending"}
         </td>
         <td className="px-3 py-2 sm:px-6 sm:py-3 ">
