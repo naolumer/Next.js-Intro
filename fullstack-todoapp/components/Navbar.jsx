3949fde6a5239a0096c10c6f2e10c5f04174687e
naolumer <naolumer3@gmail.com>
2025-07-10 20:22:54 +0300

    some changes

--- fullstack-todoapp/components/Navbar.jsx
+++ fullstack-todoapp/components/Navbar.jsx
@@ -5,6 +5,7 @@ const Navbar = () => {
     <div className='flex py-3 flex-wrap justify-around'>
         <h1 className='text-lg font-semibold'>Todo App</h1>
         <ul className='flex gap-[40px] text-m'>
+          
             <li>Home</li>
             <li>Products</li>
             <li>About</li>
